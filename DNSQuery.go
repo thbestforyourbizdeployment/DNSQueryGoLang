d5ba34a360772211709acd6db642d850674359c4
thbestforyourbizdeployment <73033172+thbestforyourbizdeployment@users.noreply.github.com>
2021-11-20 14:51:35 -0500

    Add files via upload

---
+++ DNSQuery.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func main() {
+
+	fmt.Println("EnterDomainName: ")
+
+	// var then variable name then variable type
+	var domain string
+
+	// Taking input from user
+	fmt.Scanln(&domain)
+
+	iprecords, _ := net.LookupIP(domain)
+	for _, ip := range iprecords {
+		fmt.Println(ip)
+	}
+
+	mxrecords, _ := net.LookupMX(domain)
+	for _, mx := range mxrecords {
+		fmt.Println(mx.Host, mx.Pref)
+	}
+
+	txtrecords, _ := net.LookupTXT(domain)
+
+	for _, txt := range txtrecords {
+		fmt.Println(txt)
+	}
+
+	nameserver, _ := net.LookupNS(domain)
+	for _, ns := range nameserver {
+		fmt.Println(ns)
+	}
+}
